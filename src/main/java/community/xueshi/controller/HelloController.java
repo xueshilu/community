647733daae49cc4ca2f48adfb0922fce1e5278a2
Xueshi Lu <xueshi.lu@outlook.com>
2022-07-07 12:01:25 +0100

    init repo

---
+++ src/main/java/community/xueshi/controller/HelloController.java
@@ -0,0 +1,16 @@
+package community.xueshi.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello(@RequestParam(name = "name") String n, Model model){
+        model.addAttribute("name", n);
+        return "hello";
+    }
+}
