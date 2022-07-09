4c5043f757510aa4905988c49598224a07183b8a
Xueshi Lu <xueshi.lu@outlook.com>
2022-07-09 22:01:06 +0100

    add cookies

--- src/main/java/community/xueshi/controller/AuthorizeController.java
+++ src/main/java/community/xueshi/controller/AuthorizeController.java
@@ -11,7 +11,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.util.UUID;
 
 @Controller
@@ -32,7 +34,8 @@ public class AuthorizeController {
 
     @GetMapping("/callback")
     public String callback(@RequestParam(name="code") String code,
-                           HttpServletRequest request) {
+                           HttpServletRequest request,
+                           HttpServletResponse response) {
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
         accessTokenDTO.setCode(code);
         accessTokenDTO.setRedirect_uri(redirect_uri);
@@ -43,14 +46,15 @@ public class AuthorizeController {
 
         if(githubUser != null){
             User user = new User();
-            user.setToken(UUID.randomUUID().toString());
+            //database
+            String token = UUID.randomUUID().toString();
+            user.setToken(token);
             user.setName(githubUser.getName());
             user.setAccount_id(String.valueOf(githubUser.getId()));
             user.setGmt_create(System.currentTimeMillis());
             user.setGmt_modified(user.getGmt_create());
             userMapper.insert(user);
-            //登录成功，cookie， session
-            request.getSession().setAttribute("user", githubUser);
+            response.addCookie(new Cookie("token", token));
             return "redirect:/";
         }
         else {
--- src/main/java/community/xueshi/mapper/UserMapper.java
+++ src/main/java/community/xueshi/mapper/UserMapper.java
@@ -3,10 +3,14 @@ package community.xueshi.mapper;
 import community.xueshi.model.User;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
 
 @Mapper
 public interface UserMapper {
-
     @Insert("INSERT INTO USERS (ACCOUNT_ID,NAME,TOKEN,GMT_CREATE,GMT_MODIFIED) values (#{account_id},#{name},#{token},#{gmt_create},#{gmt_modified})")
     void insert(User user);
+
+    @Select("SELECT * FROM USERS WHERE token = #{token}")
+    User findByToken(@Param("token") String token);
 }
